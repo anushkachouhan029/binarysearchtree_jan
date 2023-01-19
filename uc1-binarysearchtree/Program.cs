a027a7a3e79a76f3bb8c4c71b23995b0be0e6d0f
anushkachouhan029 <anushkachouhan029>
2023-01-19 22:43:59 +0530

    added binary search tree

---
+++ uc1-binarysearchtree/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace BinarySearchTree1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Binary Search Tree Program \n=========================================");
+            BinarySearchTree<int> BinaryTree = new BinarySearchTree<int>();
+
+            BinaryTree.AddNode(56);
+            BinaryTree.AddNode(30);
+            BinaryTree.AddNode(70);
+
+
+        }
+    }
+}
